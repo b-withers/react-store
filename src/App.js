13fde54aafc48300c7d105cfcc221a48963d3446
Brady <brady.withers@outlook.com>
2019-04-23 13:47:17 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Axios from 'axios';
+import TopNav from './components/TopNav';
+import Home from './components/Home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+	constructor(props){
+		super(props);
+		this.state = {vehicleData: null};
+	}
+
+componentDidMount(){
+	Axios.get("http://localhost:3001/vehicles")
+	.then (res => {
+		console.log(res.data);
+		this.setState({ vehicleData: res.data });
+	})
+	.catch(err => console.log(err));
+}
+
+render(){    
+	return(
+		<div className="App">
+			<TopNav />
+			<Home />
+		</div>
+	);
+}
+
 }
 
 export default App;
---
+++ src/components/TopNav/TopNav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './TopNav.css';
+
+class TopNav extends React.Component {
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return(<p>This is the NavBar</p>);
+	}
+}
+
+export default TopNav;
